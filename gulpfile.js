cc5603a510fe618bbbf3d66b89d2ec9bb73b7266
Ankit Gupta <aaygupta@outlook.com>
2024-06-18 18:30:59 +0530

    Image folder separation

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,32 @@ var sass = require('gulp-sass')(require('sass'));
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var del = require('del');
+const flatmap = require('gulp-flatmap');
+const path = require('path');
 
 gulp.task('delete', function () {
-    return del(['images/*.*']);
+    return del(['images_import/**', '!images_import']);
 });
 
 gulp.task('resize-images', function () {
-    return gulp.src('images/*.*')
-        .pipe(imageResize({
-            width: 1024,
-            imageMagick: true
-        }))
-        .pipe(gulp.dest('images/fulls'))
-        .pipe(imageResize({
-            width: 512,
-            imageMagick: true
-        }))
-        .pipe(gulp.dest('images/thumbs'));
+    return gulp.src('images_import/**/*.*')
+        .pipe(flatmap(function(stream, file) {
+            const relativePath = path.relative(file.base, file.path);
+            const destPathFulls = path.join('images/fulls', relativePath);
+            const destPathThumbs = path.join('images/thumbs', relativePath);
+
+            return gulp.src(file.path)
+                .pipe(imageResize({
+                    width: 1024,
+                    imageMagick: true
+                }))
+                .pipe(gulp.dest(path.dirname(destPathFulls)))
+                .pipe(imageResize({
+                    width: 512,
+                    imageMagick: true
+                }))
+                .pipe(gulp.dest(path.dirname(destPathThumbs)));
+        }));
 });
 
 // compile scss to css
